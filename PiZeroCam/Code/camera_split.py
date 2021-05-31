cead10b26925ab99b314d956f54d2bbee2442b66
Bhaskaryechuri <Bhaskar.yechuri@gmail.com>
2021-05-31 15:52:00 -0700

    Update camera_split.py

--- PiZeroCam/Code/camera_split.py
+++ PiZeroCam/Code/camera_split.py
@@ -1,11 +1,16 @@
+#Author: Bhaskar Yechuri (bhaskar.yechuri@gmail.com) - Altshuler Lab, 2021
+
+#This script, when run at PI Zero startup, will record video for in contiguous segments up to a maximum time limit. The length of segments and maximum recording time can be set in this script.
+#However, maximum time limit can only be increased as far as the connected battery will allow. Once battery runs out, the recording will stop and the Pi will shut down.
+
 import picamera
 import time
 import os
 import psutil
 
 #HARDCODED VALUES TO CHANGE
-time_lim = 20 #upper limit on total length of time to record for
-length_of_segment = 5 #length of each small video in seconds
+time_lim = 20 #upper limit on total length of time to record for (mins)
+length_of_segment = 5 #length of each small video (in seconds)
 
 #get start time of recording and use that as filename
 #record videos in increments of 10min to prevent data loss due to power off or battery depletion
@@ -16,7 +21,7 @@ print("Memory available = " + str(psutil.disk_usage('/').free/1024/1024/1024))
 
 #keep recording until memory runs low or time_limit hasn't been reached
 #create a new folder for each recording session, within which a number lists of sub-videos will be created
-filename = "video at " + time.strftime("%H:%M:%S",time.localtime())
+filename = "video_at_" + time.strftime("%H_%M_%S",time.localtime())
 foldername = '/home/pi/Desktop/' + filename + "/"
 if not os.path.exists(foldername):
     os.makedirs(foldername)
